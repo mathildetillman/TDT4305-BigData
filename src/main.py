5038750e6ea38af9ce27d2868ad8e43d62822b97
Erik Skår <erik.skar@online.ntnu.no>
2021-02-25 17:19:34 +0100

    Cleanup

--- src/main.py
+++ src/main.py
@@ -1,6 +1,5 @@
 from pyspark import SparkContext, SparkConf
 from pyspark.sql.context import SQLContext
-import os
 from task1 import loadDataAndPrintNumRows
 from task2 import task2
 from task3 import task3
@@ -8,7 +7,6 @@ from task3 import task3
 def main():
     conf = SparkConf().setAppName("TDT4305 Assignment 1").setMaster("local")
     sc = SparkContext(conf=conf)
-    #sc.addPyFile("../TDT4305-BigData/graphframes-0.8.1-spark3.0-s_2.12.jar")
     sqlContext = SQLContext(sc)
     # Task 1 - Load data and print number of rows
     posts, comments, users, badges = loadDataAndPrintNumRows(sc)
